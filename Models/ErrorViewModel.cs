338e7f371cb40c87a2878bfe5fdbf29ff8405b5c
Abdulmalek Saif <155893893+KingShaheen7@users.noreply.github.com>
2025-06-06 20:11:05 +0300

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace b_tech.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
