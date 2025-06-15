b249fb62182b29ce2b1ad529e9759844e2981b5b
Abdulmalek Saif <155893893+KingShaheen7@users.noreply.github.com>
2025-06-15 20:19:16 +0000

    update1

--- obj/Debug/net8.0/b-tech.AssemblyInfo.cs
+++ obj/Debug/net8.0/b-tech.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("b-tech")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9579a791de3f9be416c84ad7ca2000f00b98fc50")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94aa319500bf42c4f9b6a65a5b35d036fa08f36b")]
 [assembly: System.Reflection.AssemblyProductAttribute("b-tech")]
 [assembly: System.Reflection.AssemblyTitleAttribute("b-tech")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
